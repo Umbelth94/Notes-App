e9faf19a498716c3f91c29021890d1e5543391ba
Travis Umbel <Umbelth94@gmail.com>
2024-02-09 14:44:23 -0600

    finish html routes

--- Develop/server.js
+++ Develop/server.js
@@ -4,9 +4,16 @@ const path = require('path');
 const PORT = 3001;
 const app = express();
 
+//
 app.use(express.static('public'));
 
 app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
+
+// app.get('/notes', (req,res) => res.send(path.join(__dirname, '/public/notes.html')));
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+app.get('*', (req,res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
 //Create the following HTML routes:
     //Get /notes should return the notes.html file
